1275eb55ee7a022a5c72c5e0ac8e41ef691c9716
elm-suf <elm.sufian@gmail.com>
2020-11-19 14:49:43 +0100

    ✨ cinema crud WIP

--- src/main/java/com/spike/cinema/cinema/CinemaController.java
+++ src/main/java/com/spike/cinema/cinema/CinemaController.java
@@ -2,8 +2,11 @@ package com.spike.cinema.cinema;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +14,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cinema")
 public class CinemaController {
 
-    @Autowired
-    CinemaRepo repo;
+    private final CinemaRepo repo;
+
+    CinemaController(CinemaRepo repo) {
+        this.repo = repo;
+    }
 
     @GetMapping
-    public List<Cinema> getAll() {
+    public List<Cinema> get() {
         return repo.findAll();
     }
+
+    @PostMapping
+    public Cinema post(Cinema item) {
+        return repo.save(item);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> delete(Cinema item) {
+        var present = repo.findById(item.getId()).isPresent();
+        if (present) {
+            repo.delete(item);
+            return new ResponseEntity<>(String.format("Deleted item with id: ${%d}", item.getId()), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(String.format("No item found with id : ${%d}", item.getId()),
+                    HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
